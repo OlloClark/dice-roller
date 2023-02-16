62128d4fd71f38b1aa628e520792d52fbc892b75
OlloClark <ollo42@hotmail.co.uk>
2023-02-16 15:04:27 -0800

    adding positive and negative modifier functionality

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const btn = document.querySelector("#btn")
 const output = document.querySelector("#output")
-
-
-
 btn.addEventListener("click", diceRoll)
 
+
 function diceRoll() {
-  // let dice = document.getElementById("mySelect").value;
   const diceSize = parseInt(document.getElementById("sizeSelect").value)
   const quantitySelect = parseInt(document.getElementById("quantitySelect").value)
+  const modifier = parseInt(document.getElementById("modifier").value)
   const result = (Math.floor(Math.random() * diceSize * quantitySelect) + 1);
+  const modifiedResult = result + modifier;
     console.log(result)
     if (result === 1 && diceSize === 20) {
-      output.innerText = "You rolled a NAT 1!"
+      output.innerText = "NAT 1!"
     } else if (result === 20 && diceSize === 20) {
-      output.innerText = "You rolled a NAT 20!"
+      output.innerText = "NAT 20!"
+    } else if (modifier >= 0){
+      output.innerText = `${(result)} + ${(modifier)} = ${(modifiedResult)}.`
     } else {
-      output.innerText = `You rolled a ${(result)}`
+      output.innerText = `${(result)} - ${(Math.abs(modifier))} = ${(modifiedResult)}.`
     }
 }
