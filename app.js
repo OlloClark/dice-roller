38566ddf24a0abb261ed20e72ce835254303cefb
OlloClark <ollo42@hotmail.co.uk>
2023-02-16 14:48:43 -0800

    adding multiple dice functionality

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const btn = document.querySelector("#btn")
 const output = document.querySelector("#output")
 
+
+
 btn.addEventListener("click", diceRoll)
 
 function diceRoll() {
   // let dice = document.getElementById("mySelect").value;
-  let diceSize = parseInt(document.getElementById("mySelect").value)
-  const result = (Math.floor(Math.random() * diceSize) + 1);
+  const diceSize = parseInt(document.getElementById("sizeSelect").value)
+  const quantitySelect = parseInt(document.getElementById("quantitySelect").value)
+  const result = (Math.floor(Math.random() * diceSize * quantitySelect) + 1);
     console.log(result)
     if (result === 1 && diceSize === 20) {
       output.innerText = "You rolled a NAT 1!"
