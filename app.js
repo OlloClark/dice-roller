7acd48f50a0381283353554b7bfc1210c6466ba5
OlloClark <ollo42@hotmail.co.uk>
2023-02-16 15:43:27 -0800

    adding checkbox for advantage

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector("#btn")
 const output = document.querySelector("#output")
 const sizeSelect = document.querySelector("#sizeSelect")
+const yesAdvantage = document.getElementById("yesAdvantage")
+
 btn.addEventListener("click", diceRoll)
 sizeSelect.addEventListener("change", offerAdvantage)
 
@@ -8,13 +10,12 @@ sizeSelect.addEventListener("change", offerAdvantage)
 function offerAdvantage() {
   const diceSize = parseInt(document.getElementById("sizeSelect").value);
   diceSize === 20
-    ? document.getElementById("advantage").style.visibility = "visible"
-    : document.getElementById("advantage").style.visibility = "hidden"
+    ? document.getElementById("advantageCheck").style.visibility = "visible"
+    : document.getElementById("advantageCheck").style.visibility = "hidden"
 
 }
 
 offerAdvantage()
-
 
 function diceRoll() {
   const diceSize = parseInt(document.getElementById("sizeSelect").value)
@@ -22,14 +23,24 @@ function diceRoll() {
   const modifier = parseInt(document.getElementById("modifier").value)
   const result = (Math.floor(Math.random() * diceSize * quantitySelect) + 1);
   const modifiedResult = result + modifier;
+
     console.log(result)
-    if (result === 1 && diceSize === 20) {
+    if (yesAdvantage.checked) {
+      output.innerText = "rolling with adv!"
+    } else if (result === 1 && diceSize === 20) {
       output.innerText = "NAT 1!"
     } else if (result === 20 && diceSize === 20) {
       output.innerText = "NAT 20!"
-    } else if (modifier >= 0){
+    } else if (modifier >= 0) {
       output.innerText = `${(result)} + ${(modifier)} = ${(modifiedResult)}.`
     } else {
       output.innerText = `${(result)} - ${(Math.abs(modifier))} = ${(modifiedResult)}.`
     }
+}
+
+
+yesAdvantage.addEventListener("change", isChecked)
+
+function isChecked() {
+  yesAdvantage.checked ? console.log("checked") : console.log("unchecked");
 }
