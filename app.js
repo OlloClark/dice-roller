06166f4c92096000a841ae677340a5fd2ac606b6
OlloClark <ollo42@hotmail.co.uk>
2023-02-16 13:30:37 -0800

    basic d20 output successful

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const d6 = 6
 // const d12 = 12
 // const d20 = 20
 const btn = document.querySelector("#btn")
+const output = document.querySelector("#output")
+
+
 btn.addEventListener("click", diceRoll)
 
 function diceRoll() {
-  // btn.addEventListener("click", function(){
     console.log("clicked")
     console.log(Math.floor(Math.random() * 20) + 1)
-    // return(Math.floor(Math.random() * die) + 1)
-  // })
-  
+    output.innerText = `You rolled a ${(Math.floor(Math.random() * 20) + 1)}`
 }
 
 // console.log(`You rolled a ${diceRoll(d12)}!`)
